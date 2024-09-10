6ff967da5f057cca676333540a4775a37f91b01a
mrndolo <callistomutunga@gmail.com>
2024-09-10 03:27:23 -0400

    variables in python

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("Hello World")
 print('O----')
-print('||||')
+print(' ||||')
+print('*' * 10)
---
+++ variables.py
@@ -0,0 +1,21 @@
+#integer
+price = 10
+#float
+rating = 4.9
+#string
+name = 'Carl'
+#boolean
+is_published = True
+
+print(price)
+
+#Case sensitive for var manes
+
+full_name = 'John Smith'
+age = 20
+new_patient = True
+
+#receive variables
+name = input('What is your name? ')
+color = input('What is your favorite color? ')
+print('Hi ' + name + ', you like ' + color + ' color')
