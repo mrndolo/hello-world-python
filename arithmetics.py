72fefb852643c080627a155205c0849d74327d0b
mrndolo <callistomutunga@gmail.com>
2024-09-11 10:19:37 -0400

    Math functions

--- arithmetics.py
+++ arithmetics.py
@@ -15,3 +15,19 @@ x = 10
 #x += 3
 x -= 3
 print(x)
+#parenthisis, exponentiation, multiplication, division, addition, subtraction
+x = 10 + 3 * 2 ** 8
+print(x)
+
+#Math functions
+x = 2.9
+print(round(x))
+#abs or absolute
+# always returns the positive representation of a number even if the value is negative
+print(abs(-2.9))
+#for advanced mathe u need to import a module
+# A module is a separate file with some reusable code
+import math
+print(math.ceil(4.9))
+print(math.floor(4.9))
+#learn more at python 3 math module or docs.python.org
