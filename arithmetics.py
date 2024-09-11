763399ee80395cb165c287875238b0bb4f36f623
mrndolo <callistomutunga@gmail.com>
2024-09-10 22:18:12 -0400

    arithmetic operators

---
+++ arithmetics.py
@@ -0,0 +1,17 @@
+print(10 + 3)
+#float
+print(10 / 3)
+#integer
+print(10 // 3)
+#modulus
+print(10 % 3)
+#power
+print(10 ** 3)
+
+#assignment
+x = 10
+#x = x + 3
+#augmented assignment operator
+#x += 3
+x -= 3
+print(x)
