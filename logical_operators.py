ccd7ac1faf73f30a04702be8dd81e105eb22301b
mrndolo <callistomutunga@gmail.com>
2024-09-13 13:25:34 -0400

    comparison operators

--- logical_operators.py
+++ logical_operators.py
@@ -21,3 +21,36 @@ if has_high_income or has_good_credit:
 
 if has_good_credit and not has_criminal_record:
     print("Eligible for loan")
+
+#comparison operators
+'''
+if temperature id grater than 30
+    it's a hot day
+otherwise if it's less than 10
+    it's a cold day
+otherwise
+    it's neither hot nor cold
+'''
+temperature = 30
+
+if temperature >= 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+'''
+if name is less than 3 characters long
+    name must be at least 3 characters
+otherwise if it's more than 50 characters long
+    name can be a maximum of 50 characters
+otherwise
+    name looks good!
+'''
+name = "John"
+
+if len(name) < 3:
+    print("name must be at least 3 characters")
+elif len(name) > 50:
+    print("Name can be a maximum of 50 characters")
+else:
+    print("Looks good!")
