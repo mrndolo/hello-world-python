d00c900de091532ce3d49f91468ef407effe2e7e
mrndolo <callistomutunga@gmail.com>
2024-09-14 16:05:18 -0400

    while loop

--- while_loops.py
+++ while_loops.py
@@ -21,3 +21,48 @@ while guess_count < guess_limit:
         break
 else:
     print("Sorry, you've failed!")
+'''
+begin = input('>')
+if begin.lower() == "help":
+    print("start - to start the car")
+    print("stop - to stop the car")
+    print("quit - to exit")
+    car_status = input(">")
+    if car_status == "start":
+        print("Car started... Ready to go!")
+        car_status = input(">")
+    elif car_status == "stop":
+        print("Car stopped!...")
+        car_status = input(">")
+    elif car_status == "quit":
+        print("Ended!...")
+
+else:
+    print("I don't understand that...")
+'''
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car is already started!")
+        else:
+            started = True
+            print("Car started... Ready to go!")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print("Car stopped!...")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to exit
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry I don't understand that.")
