086d522e3d96199dbb5a733c1ba635086d1086a2
mrndolo <callistomutunga@gmail.com>
2024-09-19 23:49:24 -0400

    nested loops

---
+++ nested_loops.py
@@ -0,0 +1,18 @@
+#adding one loop inside of another loop
+
+for x in range(4):
+    for y in range(3):
+        print(f'({x}, {y})')
+
+#displaying a letter F
+
+numbers = [5,2,5,2,2]
+
+for number in numbers:
+    print('x' * number)
+
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
