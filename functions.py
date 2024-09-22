f47d84a7311cfea90903f07793075162792ddff8
mrndolo <callistomutunga@gmail.com>
2024-09-22 14:39:09 -0400

    functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+#A function is a container for a few lines of code that performs a specific task
+#def is a reserved keyword, short for define
+#we always define our functions first then call them
+def greet_user():
+    print('Hi there!')
+    print('Welcome aboard!')
+
+
+print("Start")
+greet_user()
+print("Finish")
