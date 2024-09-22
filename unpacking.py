557c355439d4f8b9aa8ab5bef53e2f17cf991407
mrndolo <callistomutunga@gmail.com>
2024-09-21 21:51:10 -0400

    unpacking

---
+++ unpacking.py
@@ -0,0 +1,12 @@
+coordinates = (1,2,3)
+'''
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+'''
+
+# to shorten the code(can be used with lists too)
+x,y,z = coordinates
+print(x)
+print(y)
+print(z)
