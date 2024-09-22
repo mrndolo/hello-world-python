0451ced54d6019fd359f8d5339f0085ab4cb8a7f
mrndolo <callistomutunga@gmail.com>
2024-09-21 21:44:55 -0400

    tuples

---
+++ tuples.py
@@ -0,0 +1,7 @@
+#tuples are similar to lists but we cannot modify them. They are immutable
+#A list
+numbers = [1,2,3]
+#A tuple
+numbers_tuples = (1,2,3)
+#accessing item in tuple
+print(numbers_tuples[0])
