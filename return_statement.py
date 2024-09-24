51c974f8b3f61aac2c403d95afdb87c9c76b6e21
mrndolo <callistomutunga@gmail.com>
2024-09-24 18:09:23 -0400

    return_statement

---
+++ return_statement.py
@@ -0,0 +1,12 @@
+#functions that return values
+def square(number):
+    print(number * number)
+#if you don't have a return statement, Python will return None
+def double(digit):
+    print(digit * digit)
+
+
+
+#or you can shorten as:
+print(square(3))
+print(double(2))
