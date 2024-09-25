0020e4262782ba97adc401f5f60b457be2a8ebd5
mrndolo <callistomutunga@gmail.com>
2024-09-24 23:20:43 -0400

    comments

---
+++ comments.py
@@ -0,0 +1,5 @@
+#we use comments to add notes to our program
+#like this!
+#use comments to explain whys and hows not whats
+#if you have made assumptions, use comments to explain those assumptions
+
